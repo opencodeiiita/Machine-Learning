018ca384e41bb9ec168e5d91aa6c9c480bbcfea1
Jaggu <khushboojagnani254@gmail.com>
2019-01-26 11:29:03 +0530

    csv Reading completed

---
+++ csvReading.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 26 11:21:42 2019
+
+@author: Manish Jagnani
+"""
+
+import pandas as pd
+
+train_data = pd.read_csv('Train.csv')
+test_data = pd.read_csv('Test.csv')
