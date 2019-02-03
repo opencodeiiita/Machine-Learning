45938dc9f9661b8668b38fb951a69709d03f81c8
Ashish Agarwal <ashishagarwal2225@gmail.com>
2019-02-03 20:37:17 +0530

    Separating train and test data

--- csvReading.py
+++ csvReading.py
@@ -7,5 +7,9 @@ Created on Sat Jan 26 11:21:42 2019
 
 import pandas as pd
 
-train_data = pd.read_csv('Train.csv')
-test_data = pd.read_csv('Test.csv')
+train_data = pd.read_csv('train.csv')
+test_data = pd.read_csv('test.csv')
+train_data_x = train_data.iloc[0:,0].values
+train_y = train_data.iloc[0:,1].values
+test_data_x = test_data.iloc[0:, 0].values
+test_y = test_data.iloc[0:, 1].values
