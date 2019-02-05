ad5e68a6724b1091bf09cefab1966d51a0d832e5
Ashish Agarwal <ashishagarwal2225@gmail.com>
2019-02-05 16:26:47 +0530

    Visualising data

--- csvReading.py
+++ csvReading.py
@@ -6,6 +6,8 @@ Created on Sat Jan 26 11:21:42 2019
 """
 
 import pandas as pd
+import matplotlib.pyplot as plt 
+from pandas.plotting import scatter_matrix
 
 train_data = pd.read_csv('train.csv')
 test_data = pd.read_csv('test.csv')
@@ -13,3 +15,21 @@ train_data_x = train_data.iloc[0:,0].values
 train_y = train_data.iloc[0:,1].values
 test_data_x = test_data.iloc[0:, 0].values
 test_y = test_data.iloc[0:, 1].values
+
+train_data.hist()
+plt.show()
+
+test_data.hist()
+plt.show()
+
+train_data.plot(kind='box', subplots=True, layout=(3,3), sharex=False, sharey=False)
+plt.show()
+
+test_data.plot(kind='box', subplots=True, layout=(3,3), sharex=False, sharey=False)
+plt.show()
+
+scatter_matrix(train_data)
+plt.show()
+
+scatter_matrix(test_data)
+plt.show()
